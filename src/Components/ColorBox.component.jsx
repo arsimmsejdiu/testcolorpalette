2bce78b1c905148792f598ef2dcb4086a4e39910
arsimmsejdiu <arsimmsejdiu@gmail.com>
2020-07-15 16:25:11 +0200

    Atyled the box color text and button

--- src/Components/ColorBox.component.jsx
+++ src/Components/ColorBox.component.jsx
@@ -1,12 +1,18 @@
 import React, { Component } from "react";
-import './ColorBox.css';
+import "./ColorBox.css";
 
 class ColorBox extends Component {
   render() {
+    const { name , background } = this.props;
     return (
-      <div style={{ background: this.props.background }} className="ColorBox">
-        <span>MORE</span>
-        <span>{this.props.name}</span>
+      <div style={{ background }} className="ColorBox">
+        <div className="Copy-container">
+          <div className="Box-content">
+            <span>{name}</span>
+          </div>
+          <button className="Copy-button">Copy</button>
+        </div>
+        <span className="See-more">More</span>
       </div>
     );
   }
